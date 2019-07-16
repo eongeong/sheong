98b700e016842a0a5cd5e13b700e05a2e05bea53
kongbai111 <1471646948@qq.com>
2019-07-16 08:44:57 +0800

    fix

--- sheong.js
+++ sheong.js
@@ -6,25 +6,24 @@
     const dev = {
         tree: [],
         command: {},
-        createTree: undefined
+        createTree: undefined,
+        isCreateFinish: false
     };
 
-    dev.command["she-text"] = function(VRElement, value, name){
-        VRElement.element.textContent = value;
-        VRElement.children = [];
-        if(name !== undefined){
-            VRElement.element.setAttribute(name, JSON.stringify(value));
+    dev.command["she-text"] = function(VRElement, value){
+        if(value !== undefined){
+            VRElement.element.textContent = value;
+            VRElement.children = [];
         }
     }
 
-    dev.command["she-html"] = function(VRElement, value, name){
-        VRElement.element.innerHTML = value;
-        VRElement.children = [];
-        if( value.search( new RegExp('<[A-Za-z\/]+>', 'g') ) !== -1 ){
-            dev.createTree(VRElement.element.children, VRElement.children, dev.command);
-        }
-        if(name !== undefined){
-            VRElement.element.setAttribute(name, JSON.stringify(value));
+    dev.command["she-html"] = function(VRElement, value){
+        if(value !== undefined){
+            VRElement.element.innerHTML = value;
+            VRElement.children = [];
+            if( value.search( new RegExp('<[A-Za-z\/]+>', 'g') ) !== -1 ){
+                dev.createTree(VRElement.element.children, VRElement.children, dev.commands);
+            }
         }
     }
 
@@ -48,18 +47,17 @@
                             data: [],
                             children: []
                         });
+                        if(elements[i].children.length > 0){
+                            dev.createTree(elements[i].children, tree[tree.length - 1].children, commands);
+                        }
                         isHasOneCommand = true;
                     }
 
                     tree[tree.length - 1].name[j] = elements[i].getAttribute(command);
                     tree[tree.length - 1].command[j] = command;
-                    tree[tree.length - 1].data[j] = tree[tree.length - 1].element.getAttribute(tree[tree.length - 1].name[j]);
 
-                    dev.command[command](tree[tree.length - 1], JSON.parse(tree[tree.length - 1].data[j]), tree[tree.length - 1].name[j]);
+                    dev.command[command](tree[tree.length - 1], tree[tree.length - 1].data[j], tree[tree.length - 1].name[j]);
 
-                    if(elements[i].children.length > 0){
-                        dev.createTree(elements[i].children, tree[tree.length - 1].children, commands);
-                    }
                     j++;
                 }
             }
@@ -77,28 +75,64 @@
         return function (parameter) {
             let j = 0;
 
-            const enumerable = function (elementname,tree) {
+            const enumerable = function (elementname, tree) {
                 let i = 0;
-                while ( i < tree.length && elementname !== undefined ){
+                while ( i < tree.length){
                     if( names[names.length - 1] === names[j] ){
                         let k = 0;
                         while ( k < tree[i].name.length ){
                             if( names[names.length - 1] === tree[i].name[k] ){
-
+                                tree[i].data[k] = parameter;
+                                dev.command[tree[i].command[k]](tree[i], tree[i].data[k], names[names.length - 1]);
                             }
 
                             k++;
                         }
+                    }
 
+                    if( tree[i].children.length > 0 ){
+                        let k = 0;
+                        while( k < tree[i].name.length ){
+                            if(elementname === tree[i].name[k]){
+                                j++;
+                                break;
+                            } 
+                            k++;
+                        }
+                        enumerable(names[j], tree[i].children);
+                        k = 0;
+                        while( k < tree[i].name.length ){
+                            if(elementname === tree[i].name[k]){
+                                j--;
+                                break;
+                            }
+                            k++;
+                        }
                     }
 
                     i++;
                 }
             }
 
+            const timer = setInterval(function(){
+                console.log(dev.tree);
+                if(dev.isCreateFinish){
+                    clearInterval(timer);
+                    enumerable(names[j], dev.tree);
+                }
+            },1);
 
+            return she;
         }
     }
+
+    const DOMLoad = function(){
+        const elements = document.body.children;
+        dev.createTree(elements, dev.tree, dev.command);
+        dev.isCreateFinish = true;
+        document.removeEventListener('DOMContentLoaded', DOMLoad);
+    }
+    document.addEventListener('DOMContentLoaded', DOMLoad);
 
     return she;
 });
